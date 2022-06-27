ed8ec4bbbaaa276b4c996e71266e3d2b464030ac
DMvnGaming <107683097+DMvnGaming@users.noreply.github.com>
2022-06-27 17:04:56 +0700

    Add files via upload

---
+++ hieu.py
@@ -0,0 +1,13 @@
+
+you = "hello"
+
+if you == "":
+	robot_brain = "I can't hear you, Try agin"
+elif you == "hello":
+	robot_brain = "Hello Trung Nguyen"
+elif you == "today":
+	robot_brain = "Today is Wednesday"
+else:
+	robot_brain = "I'm fine thank you and you"
+
+print(robot_brain)
